34a57ba8d467ca246ea391bd0299a2e096425fa4
yuanshuai <12344@qq.com>
2020-03-11 00:41:20 +0900

    first commit

---
+++ ABenNet.NetCore.WebApi/Extension/Swagger/RemoveVersionFromParameter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.JsonPatch.Operations;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+namespace ABenNet.NetCore.WebApi.Extension.Swagger
+{
+    public class RemoveVersionFromParameter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var versionParameter = operation.Parameters.FirstOrDefault(p => p.Name == "version");
+            operation.Parameters.Remove(versionParameter);
+        }
+    }
+}
